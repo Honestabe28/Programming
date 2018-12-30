99d889930c5800defabe6e2d0e99b23699c98c5d
WASHINGTON Abraham <53501@2021-53501.local>
2018-12-30 11:48:51 +0700

    Form

---
+++ exercise.js
@@ -0,0 +1,8 @@
+function Result(){
+var a =	document.getElementById("Name-1").value;
+var b =	document.getElementById("Name-2").value;
+var c =	document.getElementById("Date").value;
+var d = document.getElementById("Food").value;
+
+document.getElementById("OnClick").innerHTML=" My name is " + a +" "+ b + " and i was born on" + c + ". My favorite food is " + d;
+}
